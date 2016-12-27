1afda2b75f425a3bc073661f7e2a14a620f8bdb5
Fabio Hirata <fabiohh@yahoo-inc.com>
2016-12-26 18:53:14 -0800

    Adding widget.

--- app/src/main/java/com/udacity/stockhawk/ui/StockAdapter.java
+++ app/src/main/java/com/udacity/stockhawk/ui/StockAdapter.java
@@ -77,7 +77,6 @@ public class StockAdapter extends RecyclerView.Adapter<StockAdapter.StockViewHol
 
         cursor.moveToPosition(position);
 
-
         holder.symbol.setText(cursor.getString(Contract.Quote.POSITION_SYMBOL));
         holder.price.setText(dollarFormat.format(cursor.getFloat(Contract.Quote.POSITION_PRICE)));
 
